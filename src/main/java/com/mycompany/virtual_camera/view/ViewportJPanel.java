dbb6cc3997e47d0e573298f6112ed59873f83cf9
Pawel Mac <Pawel.Mac@gmail.com>
2018-01-11 18:16:54 +0100

    Updated ViewportJPanel. Edge display in several ways. TODO: choose one of them.

--- src/main/java/com/mycompany/virtual_camera/view/ViewportJPanel.java
+++ src/main/java/com/mycompany/virtual_camera/view/ViewportJPanel.java
@@ -1,10 +1,11 @@
 package com.mycompany.virtual_camera.view;
 
-import com.mycompany.virtual_camera.model.Line2DHolder;
+import com.mycompany.virtual_camera.model.Edge3D;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.Path2D;
 import java.util.Collection;
 import java.util.Observable;
 import java.util.Observer;
@@ -16,21 +17,17 @@ import javax.swing.JPanel;
  */
 public final class ViewportJPanel extends JPanel implements Observer {
     
-    private Collection<Line2DHolder> line2DHoldersCollection;
+    private Collection<Edge3D> edge3DsCollection;
     
     public ViewportJPanel(int width, int height) {
         this.setPreferredSize(new Dimension(width, height));
         this.setBackground(Color.white);
     }
     
-    // Getter and Setter
+    // Setter
     
-    public Collection<Line2DHolder> getLine2DHoldersCollection() {
-        return line2DHoldersCollection;
-    }
-    
-    public void setLine2DHoldersCollection(Collection<Line2DHolder> line2DHoldersCollection) {
-        this.line2DHoldersCollection = line2DHoldersCollection;
+    public void setEdge3DsCollection(Collection<Edge3D> edge3DsCollection) {
+        this.edge3DsCollection = edge3DsCollection;
     }
     
     // Methods
@@ -39,14 +36,38 @@ public final class ViewportJPanel extends JPanel implements Observer {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2D = (Graphics2D) g;
-        if (line2DHoldersCollection != null) {
-            for (Line2DHolder line2DHolder : line2DHoldersCollection) {
-                if (line2DHolder.isFirstInFrontOfViewport() || line2DHolder.isSecondInFrontOfViewport()) {
-                    g2D.draw(line2DHolder.getLine2D());
+        if (edge3DsCollection != null) {
+            Path2D path2D = new Path2D.Double();
+            for (Edge3D edge3D : edge3DsCollection) {
+                /* 1 way
+                if (edge3D.getFirst().isInFrontOfViewport() ||  edge3D.getSecond().isInFrontOfViewport()) {
+                    g2D.draw(edge3D.getLine2D());
                 }
+                //*/
+                
+                //* 2 way
+                if (edge3D.getFirstMock().isInFrontOfViewport() &&  edge3D.getSecondMock().isInFrontOfViewport()) {
+                    g2D.draw(edge3D.getLine2D());
+                }
+                //*/
+                
+                /* 3 way Path2D
+                if (edge3D.getFirstMock().isInFrontOfViewport() &&  edge3D.getSecondMock().isInFrontOfViewport()) {
+                    path2D.moveTo(edge3D.getLine2D().getX1(), edge3D.getLine2D().getY1());
+                    path2D.lineTo(edge3D.getLine2D().getX2(), edge3D.getLine2D().getY2());
+                    g2D.draw(path2D);
+                }
+                //*/
+                
+                /* 4 way Path2D
+                if (edge3D.getFirstMock().isInFrontOfViewport() &&  edge3D.getSecondMock().isInFrontOfViewport()) {
+                    path2D.append(edge3D.getLine2D(), false);
+                    g2D.draw(path2D);
+                }
+                //*/
             }
         } else {
-            System.out.println("line2DHoldersCollection ==  null");
+            System.out.println("edge3DsCollection == null");
         }
     }
     
